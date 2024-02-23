b817e0e2c4e834806ec3a684601fcd3671c774b4
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 16:08:43 -0300

    Alteração do listener de usuário cadastrado para emitir exceção caso o campo e-mail esteja vazio, no projeto consumer.

--- spring-rabbitmq-demo-consumer/src/main/java/com/fujikawa/springrabbitmqdemoconsumer/amqp/UsuarioCadastradoListener.java
+++ spring-rabbitmq-demo-consumer/src/main/java/com/fujikawa/springrabbitmqdemoconsumer/amqp/UsuarioCadastradoListener.java
@@ -11,6 +11,10 @@ public class UsuarioCadastradoListener {
     @RabbitListener(queues = "usuario.cadastrado.enviaremail")
     public void receberMensagem(UsuarioDTO usuario) {
         
+        if (usuario.getEmail().isEmpty()) {
+            throw new RuntimeException("E-mail vazio");
+        }
+
         String mensagem = String.format(
             "Usuário Id '%s' cadastrado com login '%s'...\n" + 
             "Enviando e-mail com dados de acesso para %s...",
