4559dacfa652ec5ed07692f3b6918f9a339aeae1
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 11:02:27 -0300

    Criação da entidade Usuario.

---
+++ src/main/java/com/fujikawa/springrabbitmqdemo/entities/Usuario.java
@@ -0,0 +1,38 @@
+package com.fujikawa.springrabbitmqdemo.entities;
+
+import java.util.UUID;
+
+public class Usuario {
+
+    private UUID id;
+
+    private String login;
+
+    private String senha;
+
+    private String email;
+
+    public Usuario(String login, String senha, String email) {
+
+        this.id = UUID.randomUUID();
+        this.login = login;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
