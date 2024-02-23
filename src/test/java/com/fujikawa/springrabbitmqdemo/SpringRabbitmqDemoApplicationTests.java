20a574a076c1ae0f0cf4db08956e90f0cd7926d9
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 10:19:01 -0300

    Criação do projeto

---
+++ src/test/java/com/fujikawa/springrabbitmqdemo/SpringRabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springrabbitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
