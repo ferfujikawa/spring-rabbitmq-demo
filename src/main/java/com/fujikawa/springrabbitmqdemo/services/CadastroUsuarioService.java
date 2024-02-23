fdd8581bd6857e9106ca5f23079e68f4b2331b41
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 13:06:46 -0300

    Criação da classe de serviço para cadastro de usuário.

---
+++ src/main/java/com/fujikawa/springrabbitmqdemo/services/CadastroUsuarioService.java
@@ -0,0 +1,32 @@
+package com.fujikawa.springrabbitmqdemo.services;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+import com.fujikawa.springrabbitmqdemo.entities.Usuario;
+
+@Service
+public class CadastroUsuarioService {
+
+    private RabbitTemplate rabbitTemplate;
+
+    public CadastroUsuarioService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public Usuario cadastrar(Usuario usuario) {
+
+        enviarParaRabbit(usuario);
+
+        return usuario;
+    }
+
+    private void enviarParaRabbit(Usuario usuario) {
+
+        String dados = usuario.toString();
+        System.out.println(dados);
+        Message mensagem = new Message(dados.getBytes());
+        rabbitTemplate.send("usuario.cadastrado", mensagem);
+    }
+}
