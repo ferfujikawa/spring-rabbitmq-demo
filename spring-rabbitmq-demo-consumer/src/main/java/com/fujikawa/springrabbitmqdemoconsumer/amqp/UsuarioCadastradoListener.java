60586d88fb05ea72c6f637343221f5da5753efb4
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 14:54:15 -0300

    Alteração do listener UsuarioCadastradoListener para receber um objeto da classe UsuarioDTO.

--- spring-rabbitmq-demo-consumer/src/main/java/com/fujikawa/springrabbitmqdemoconsumer/amqp/UsuarioCadastradoListener.java
+++ spring-rabbitmq-demo-consumer/src/main/java/com/fujikawa/springrabbitmqdemoconsumer/amqp/UsuarioCadastradoListener.java
@@ -1,14 +1,22 @@
 package com.fujikawa.springrabbitmqdemoconsumer.amqp;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import com.fujikawa.springrabbitmqdemoconsumer.dtos.UsuarioDTO;
 
 @Component
 public class UsuarioCadastradoListener {
 
     @RabbitListener(queues = "usuario.cadastrado")
-    public void receberMensagem(Message mensagem) {
-        System.out.println("Recebi a mensagem: " + mensagem.toString());
+    public void receberMensagem(UsuarioDTO usuario) {
+        
+        String mensagem = String.format(
+            "Usuário Id '%s' cadastrado com login '%s'...\n" + 
+            "Enviando e-mail com dados de acesso para %s...",
+            usuario.getId(),
+            usuario.getLogin(),
+            usuario.getEmail());
+        System.out.println(mensagem);
     }
 }
