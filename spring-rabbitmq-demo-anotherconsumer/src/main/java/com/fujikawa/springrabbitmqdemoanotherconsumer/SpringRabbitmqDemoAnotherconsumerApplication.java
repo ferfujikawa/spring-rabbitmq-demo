a37360426192781b70d1fc0c1590de572da143a4
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 15:04:17 -0300

    Criação do projeto anotherconsumer.

---
+++ spring-rabbitmq-demo-anotherconsumer/src/main/java/com/fujikawa/springrabbitmqdemoanotherconsumer/SpringRabbitmqDemoAnotherconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springrabbitmqdemoanotherconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqDemoAnotherconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitmqDemoAnotherconsumerApplication.class, args);
+	}
+
+}
---
+++ spring-rabbitmq-demo-anotherconsumer/src/test/java/com/fujikawa/springrabbitmqdemoanotherconsumer/SpringRabbitmqDemoAnotherconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springrabbitmqdemoanotherconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqDemoAnotherconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
