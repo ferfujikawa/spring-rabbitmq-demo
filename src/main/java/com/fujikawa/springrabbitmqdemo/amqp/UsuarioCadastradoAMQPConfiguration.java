d5e0b717d716b88282672d0e5c5a90ead040bbc8
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 11:11:33 -0300

    Criação da classe de configuração da fila de usuario.cadastrado no RabbitMQ.

---
+++ src/main/java/com/fujikawa/springrabbitmqdemo/amqp/UsuarioCadastradoAMQPConfiguration.java
@@ -0,0 +1,29 @@
+package com.fujikawa.springrabbitmqdemo.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UsuarioCadastradoAMQPConfiguration {
+
+    @Bean
+    public Queue criarFila() {
+        return QueueBuilder.nonDurable("usuario.cadastrado").build();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+}
