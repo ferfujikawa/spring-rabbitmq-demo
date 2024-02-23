a7d6626da4404047e707f6f38a7d51094f444803
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 15:26:44 -0300

    Criação da classe UsuarioDTO no projeto anotherconsumer.

---
+++ spring-rabbitmq-demo-anotherconsumer/src/main/java/com/fujikawa/springrabbitmqdemoanotherconsumer/dtos/UsuarioDTO.java
@@ -0,0 +1,39 @@
+package com.fujikawa.springrabbitmqdemoanotherconsumer.dtos;
+
+import java.util.UUID;
+
+public class UsuarioDTO {
+
+    private UUID id;
+
+    private String login;
+
+    private String senha;
+
+    private String email;
+
+    protected UsuarioDTO() {}
+
+    public UsuarioDTO(UUID id, String login, String senha, String email) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
