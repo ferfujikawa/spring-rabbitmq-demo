34579246e543a15e75fc2771eae28910ccf08279
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 13:48:33 -0300

    Rollback da alteração da classe de start do projeto consumer para implementar CommandLineRunner.

--- spring-rabbitmq-demo-consumer/src/main/java/com/fujikawa/springrabbitmqdemoconsumer/SpringRabbitmqDemoConsumerApplication.java
+++ spring-rabbitmq-demo-consumer/src/main/java/com/fujikawa/springrabbitmqdemoconsumer/SpringRabbitmqDemoConsumerApplication.java
@@ -1,19 +1,13 @@
 package com.fujikawa.springrabbitmqdemoconsumer;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRabbitmqDemoConsumerApplication implements CommandLineRunner {
+public class SpringRabbitmqDemoConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRabbitmqDemoConsumerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
 	}
 
 }
