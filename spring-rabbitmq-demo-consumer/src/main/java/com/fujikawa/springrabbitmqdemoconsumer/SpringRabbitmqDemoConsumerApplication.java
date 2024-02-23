f7904cf363efa851afccc8605973bb16600bdf06
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 13:42:08 -0300

    Criação do projeto consumer.

---
+++ spring-rabbitmq-demo-consumer/src/main/java/com/fujikawa/springrabbitmqdemoconsumer/SpringRabbitmqDemoConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springrabbitmqdemoconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqDemoConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitmqDemoConsumerApplication.class, args);
+	}
+
+}
---
+++ spring-rabbitmq-demo-consumer/src/test/java/com/fujikawa/springrabbitmqdemoconsumer/SpringRabbitmqDemoConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springrabbitmqdemoconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqDemoConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
