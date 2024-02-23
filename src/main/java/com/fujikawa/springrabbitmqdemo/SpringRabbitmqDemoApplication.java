02ef6f8733880226407fb9bee2054c71441e5b02
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 13:07:43 -0300

    Alteração da classe de start do projeto para implementar CommandLineRunner e realizar a chamada ao método cadastrar  do serviço de cadastro de usuário.

--- src/main/java/com/fujikawa/springrabbitmqdemo/SpringRabbitmqDemoApplication.java
+++ src/main/java/com/fujikawa/springrabbitmqdemo/SpringRabbitmqDemoApplication.java
@@ -1,13 +1,35 @@
 package com.fujikawa.springrabbitmqdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fujikawa.springrabbitmqdemo.entities.Usuario;
+import com.fujikawa.springrabbitmqdemo.services.CadastroUsuarioService;
+
 @SpringBootApplication
-public class SpringRabbitmqDemoApplication {
+public class SpringRabbitmqDemoApplication implements CommandLineRunner {
+
+	private CadastroUsuarioService service;
+
+	public SpringRabbitmqDemoApplication(CadastroUsuarioService service) {
+		this.service = service;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRabbitmqDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		cadastrarUsuario();
+	}
+
+	private void cadastrarUsuario() {
+		
+		Usuario usuario = new Usuario("Usuário 3", "padrao123", "usuario1@email.com");
+		service.cadastrar(usuario);
 	}
 
 }
