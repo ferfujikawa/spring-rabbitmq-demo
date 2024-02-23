bfd57079e9f47949a6e4676cae10ef709e2d5db5
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-23 15:24:46 -0300

    Adição de Beans para criação da Queue e FanoutExchange para o projeto consumer, além de Beans para binding da queue com a fanoutExchange e demais beans para gerenciamento do RabbitMQ.

--- spring-rabbitmq-demo-consumer/src/main/java/com/fujikawa/springrabbitmqdemoconsumer/amqp/UsuarioCadastroAMQPConfiguration.java
+++ spring-rabbitmq-demo-consumer/src/main/java/com/fujikawa/springrabbitmqdemoconsumer/amqp/UsuarioCadastroAMQPConfiguration.java
@@ -1,8 +1,17 @@
 package com.fujikawa.springrabbitmqdemoconsumer.amqp;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +31,39 @@ public class UsuarioCadastroAMQPConfiguration {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue fila() {
+
+        return QueueBuilder
+            .nonDurable("usuario.cadastrado.enviaremail")
+            .build();
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+
+        return ExchangeBuilder
+            .fanoutExchange("usuario.cadastrado")
+            .build();
+    }
+
+    @Bean
+    public Binding binding() {
+
+        return BindingBuilder
+            .bind(fila())
+            .to(fanoutExchange());
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
     }
 }
